475e250a24b7c5bfbc16b51967e673e256157bab
Yash-5865 <yashamin14@gmail.com>
2025-05-08 13:14:42 -0400

    minor tf edits

--- Terraform/data.tf
+++ Terraform/data.tf
@@ -1,8 +1,8 @@
-data "openstack_networking_network_v2" "sharednet1" {
-  name = "sharednet1"
+data "openstack_networking_network_v2" "sharednet3" {
+  name = "sharednet3"
 }
-data "openstack_networking_subnet_v2" "sharednet1_subnet" {
-  name = "sharednet1-subnet"
+data "openstack_networking_subnet_v2" "sharednet3_subnet" {
+  name = "sharednet3-subnet"
 }
 
 data "openstack_networking_secgroup_v2" "allow_ssh" {

b0a80b780566594a458ec780f1ee5cbef9e830a6
Yash-5865 <yashamin14@gmail.com>
2025-05-08 13:22:28 -0400

    minor tf edits

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "suffix" {
-  description = "Net ID"
+  description = "Suffix for resource names (use net ID)"
   type        = string
   nullable = false
 }
@@ -7,12 +7,14 @@ variable "suffix" {
 variable "key" {
   description = "Name of key pair"
   type        = string
-  default     = "default"
+  default     = "id_rsa_chameleon"
 }
 
 variable "nodes" {
   type = map(string)
   default = {
     "node1" = "192.168.1.11"
+    "node2" = "192.168.1.12"
+    "node3" = "192.168.1.13"
   }
 }
